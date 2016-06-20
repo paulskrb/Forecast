4d19e85c952d98fb8e8d8e318f73779002801a32
wubalubadubdub <beargrillsllc@gmail.com>
2016-06-20 14:48:21 -0400

    fixed bolding of pressure, feels like, uv, and actual data is now displayed for those

--- app/src/main/java/com/example/bearg/forecast/fragments/ConditionsFragment.java
+++ app/src/main/java/com/example/bearg/forecast/fragments/ConditionsFragment.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import com.example.bearg.forecast.R;
 import com.example.bearg.forecast.interfaces.WeatherApiManager;
 import com.example.bearg.forecast.model.currentconditions.CurrentConditions;
+import com.example.bearg.forecast.model.currentconditions.CurrentObservation;
 import com.squareup.picasso.Picasso;
 
 import java.util.regex.Matcher;
@@ -39,6 +40,9 @@ public class ConditionsFragment extends Fragment {
     private TextView windSpeedTv;
     private TextView relativeHumidityTv;
     private TextView dewPointTv;
+    private TextView uvTv;
+    private TextView pressureTv;
+    private TextView feelsLikeTv;
 
     public ConditionsFragment() {
         // Required empty public constructor
@@ -63,12 +67,15 @@ public class ConditionsFragment extends Fragment {
         windSpeedTv = (TextView) view.findViewById(R.id.wind_speed);
         relativeHumidityTv = (TextView) view.findViewById(R.id.rel_humidity);
         dewPointTv = (TextView) view.findViewById(R.id.dewpoint);
+        uvTv = (TextView) view.findViewById(R.id.uv_index);
+        pressureTv = (TextView) view.findViewById(R.id.pressure);
+        feelsLikeTv = (TextView) view.findViewById(R.id.feelsLikeF);
 
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.conditions_refresh);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                // currently, we just cancel the loading animation when the user refreshes
+                loadConditions();
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
@@ -77,10 +84,10 @@ public class ConditionsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        loadForecast();
+        loadConditions();
     }
 
-    private void loadForecast() {
+    private void loadConditions() {
 
         Observable<CurrentConditions> observation =
                 WeatherApiManager.getWeatherService().getObservation("27909");
@@ -103,44 +110,58 @@ public class ConditionsFragment extends Fragment {
 
                     @Override
                     public void onNext(final CurrentConditions currentConditions) {
-
-                        String iconUrl = currentConditions.currentObservation.iconUrl;
-                        Picasso.with(getContext()).load(iconUrl).resize(150, 150)
-                                .error(android.R.drawable.ic_dialog_alert).into(currentWeatherIcon);
-
-                        String observationTime = currentConditions.currentObservation.observationTime;
-
-                        // will match ?X:XX PM/AM, where ? means an optional first digit
-                        String regex = "\\d?\\d:\\d{2}\\s[AP]M";
-
-                        Pattern pattern = Pattern.compile(regex);
-                        Matcher matcher = pattern.matcher(observationTime);
-                        if (matcher.find()) {
-                            // matcher.group(0) gets the matched portion, ?X:XX PM/AM in this case
-                            updatedTimeTv.setText(
-                                    String.format("Updated %s", matcher.group(0)));
-                        }
-
-                        else {
-                            updatedTimeTv.setText("Update Time Unknown");
-                        }
-
-                        currentWeatherTv.setText(currentConditions.currentObservation.weather);
-
-                        // U+00B0 is the Unicode code point for the degree symbol
-                        String tempF = String.valueOf(Math.round(currentConditions.currentObservation.tempF)) + "\u00b0";
-                        tempTv.setText(tempF);
-
-                        String windSpeed = String.valueOf(Math.round(currentConditions.currentObservation.windMph)) + " mph";
-                        windSpeedTv.setText(windSpeed);
-
-                        relativeHumidityTv.setText(currentConditions.currentObservation.relativeHumidity);
-
-                        String dewpointF = String.valueOf(Math.round(currentConditions.currentObservation.dewpointF)) + "\u00b0";
-                        dewPointTv.setText(dewpointF);
-
+                        loadConditions(currentConditions);
                     }
                 });
+
+    }
+
+    private void loadConditions(CurrentConditions currentConditions) {
+        final CurrentObservation currentObservation = currentConditions.currentObservation;
+        String iconUrl = currentObservation.iconUrl;
+        Picasso.with(getContext()).load(iconUrl).resize(150, 150)
+                .error(android.R.drawable.ic_dialog_alert).into(currentWeatherIcon);
+
+        String observationTime = currentObservation.observationTime;
+
+        // will match ?X:XX PM/AM, where ? means an optional first digit
+        String regex = "\\d?\\d:\\d{2}\\s[AP]M";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(observationTime);
+        if (matcher.find()) {
+            // matcher.group(0) gets the matched portion, ?X:XX PM/AM in this case
+            updatedTimeTv.setText(
+                    String.format("Updated %s", matcher.group(0)));
+        }
+
+        else {
+            updatedTimeTv.setText("Update Time Unknown");
+        }
+
+        currentWeatherTv.setText(currentObservation.weather);
+
+        // U+00B0 is the Unicode code point for the degree symbol
+        String tempF = String.valueOf(Math.round(currentObservation.tempF)) + "\u00b0";
+        tempTv.setText(tempF);
+
+        String windSpeed = String.valueOf(Math.round(currentObservation.windMph)) + " mph";
+        windSpeedTv.setText(windSpeed);
+
+        relativeHumidityTv.setText(currentObservation.relativeHumidity);
+
+        String dewpointF = String.valueOf(Math.round(currentObservation.dewpointF)) + "\u00b0";
+        dewPointTv.setText(dewpointF);
+
+        // convert uv from a string like "2.0" to just "2". double backslash is needed to escape
+        // the period, because period has special meaning in a regex
+        String uv = currentObservation.uV.split("\\.")[0];
+        uvTv.setText(uv);
+
+        pressureTv.setText(currentObservation.pressureIn);
+
+        String feelsLikeF = currentObservation.feelslikeF + "\u00b0";
+        feelsLikeTv.setText(feelsLikeF);
 
     }
 }
